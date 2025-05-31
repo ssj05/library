51ac471c0bd8f4ccb365e7306ec5396d63a538e5
ssj05 <89268633+ssj05@users.noreply.github.com>
2025-05-31 17:36:21 +0530

    add addBook and deleteBook

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const dialog = document.querySelector("dialog");
 const formDial = document.querySelector(".add");
-const addBook = document.querySelector("#addBook");
+const addBook = document.querySelector("form");
+const deleteBook = document.querySelectorAll(".delete");
+const bookContainer = document.querySelector(".all-books");
 
+// array containing all of the books
 let myLibrary = [];
 
+// book constructor
 function Book(title, author, pages, status) {
   if (!new.target) {
     throw Error("use new to construct the object");
@@ -15,19 +19,12 @@ function Book(title, author, pages, status) {
   this.uid = crypto.randomUUID();
 }
 
-const haris = new Book("rameshChodu", "me", 69, false);
-myLibrary.push(haris);
-
-formDial.addEventListener("click", () => {
-  dialog.showModal();
-});
-
-addBook.addEventListener("click", () => { });
-
-// function updateMylibrary() { }
-
+const tempBook = new Book("title", "author", "pages", true);
+myLibrary.push(tempBook);
+// Updating book view
 function updateBookView() {
-  const bookContainer = document.querySelector(".all-books");
+  // checkes if the bookContainer contains atleast one child and removing it
+  // this loop will run till all childs are removed
   while (bookContainer.lastElementChild) {
     bookContainer.removeChild(bookContainer.lastElementChild);
   }
@@ -36,6 +33,7 @@ function updateBookView() {
     return 0;
   }
 
+  // creating html book structure
   myLibrary.forEach((element) => {
     const book = document.createElement("div");
     book.setAttribute("class", "book");
@@ -53,9 +51,18 @@ function updateBookView() {
     book.appendChild(pages);
 
     const deleteBtn = document.createElement("button");
+    deleteBtn.setAttribute("id", `${element.uid}`);
     deleteBtn.setAttribute("class", "delete");
     deleteBtn.setAttribute("type", "button");
     deleteBtn.innerText = "X";
+    deleteBtn.addEventListener("click", () => {
+      myLibrary.forEach((element, index) => {
+        if (element.uid === deleteBtn.id) {
+          myLibrary.splice(index, 1);
+          updateBookView();
+        }
+      });
+    });
     book.appendChild(deleteBtn);
 
     const readStatus = document.createElement("select");
@@ -76,5 +83,28 @@ function updateBookView() {
     bookContainer.appendChild(book);
   });
 }
-
 updateBookView();
+
+formDial.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+// Adding books to myLibrary[]
+addBook.addEventListener("submit", (event) => {
+  const form = event.target;
+  const title = form.elements["title"].value;
+  const author = form.elements["author"].value;
+  const pages = form.elements["pages"].value;
+  const flag = form.elements["status"].value;
+  let status = false;
+  if (flag === "yes") {
+    status = true;
+  } else {
+    status = false;
+  }
+
+  const book = new Book(title, author, pages, status);
+  myLibrary.push(book);
+  updateBookView();
+  dialog.close();
+});
